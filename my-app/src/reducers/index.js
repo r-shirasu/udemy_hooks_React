d6aba5e3ac16f5b8726f4f1cb8f214fcd0a81129
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-28 17:38:40 +0900

    create reducer for events

--- my-app/src/reducers/index.js
+++ my-app/src/reducers/index.js
@@ -1,7 +1,10 @@
 export const events = (state = [], action) => {
     switch(action.type) {
         case 'CREATE_EVENT':
-            return state
+            const event = {title: action.title, body: action.body}
+            const length = state.length
+            let id = length === 0 ? 1 : state[length - 1].id + 1
+            return [...state, { id: id, ...event}]
         case 'DELETE_EVENT':
             return state
         case 'DELETE_ALLEVENTS':
