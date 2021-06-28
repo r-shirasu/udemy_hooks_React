31360d9f4c4d24ad46f6392cc88df7582a941038
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-28 17:15:10 +0900

    create event form and event table with bootstrap

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -11,7 +11,30 @@ function App() {
           <label htmlFor="formEventTitle">タイトル</label>
           <input className="form-control" id="formEventTitle"></input>
         </div>
+
+        <div className="form-group">
+          <label htmlFor="formEventBody">ボディー</label>
+          <textarea className="form-control" id="formEventBody"></textarea>
+        </div>
+
+        <button className='btn  btn-primary'>イベントを作成する</button>
+        <button className='btn  btn-danger'>全てのイベントを削除する</button>
       </form>
+
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>タイトル</th>
+        <th>ボディー</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+      </table>
     </div>
   );
 }
