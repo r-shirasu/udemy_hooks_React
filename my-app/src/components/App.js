1a4eb88bf3209a7ebc5b96e8fd039dd998f67f80
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-07-01 11:34:16 +0900

    stateとdispatchをpropで渡すまで

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,78 +1,18 @@
-import React, { useReducer, useState } from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 import reducer from "../reducers";
 
 import Event from "./Event";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { EventForm } from "./EventForm";
+
 function App() {
   const [state, dispatch] = useReducer(reducer, []);
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-
-  const addEvent = (e) => {
-    e.preventDefault();
-
-    dispatch({
-      type: "CREATE_EVENT",
-      title,
-      body,
-    });
-
-    setTitle("");
-    setBody("");
-  };
-
-  const deleteAllEvents = (e) => {
-    e.preventDefault();
-    const result = window.confirm(
-      "全てのイベントを本当に削除しても良いですか？"
-    );
-    if (result) dispatch({ type: "DELETE_ALLEVENTS" });
-  };
-
-  const unCreatable = title === "" || body === "";
 
   return (
     <div className="container-fluid">
-      <h4>イベント作成ホーム</h4>
-      <form>
-        <div className="form-group">
-          <label htmlFor="formEventTitle">タイトル</label>
-          <input
-            className="form-control"
-            id="formEventTitle"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          ></input>
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="formEventBody">ボディー</label>
-          <textarea
-            className="form-control"
-            id="formEventBody"
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-          ></textarea>
-        </div>
-
-        <button
-          className="btn btn-primary"
-          onClick={addEvent}
-          disabled={unCreatable}
-        >
-          イベントを作成する
-        </button>
-        <button
-          className="btn  btn-danger"
-          onClick={deleteAllEvents}
-          disabled={state.length === 0}
-        >
-          全てのイベントを削除する
-        </button>
-      </form>
-
+      <EventForm state={state} dispatch={dispatch} />
       <h4>イベント一覧</h4>
       <table className="table table-hover">
         <thead>
