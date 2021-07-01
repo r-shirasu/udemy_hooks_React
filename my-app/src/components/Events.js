446a8945d08a9c3c44f3ad5518325801901805ce
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-07-01 23:19:11 +0900

    古いcontextのやり方。consumerコンポーネントを作らないといけない

--- my-app/src/components/Events.js
+++ my-app/src/components/Events.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 import Event from "./Event";
+import { AppContext } from "../contexts/AppContext";
 
 export const Events = ({ state, dispatch }) => {
   return (
     <>
+      <AppContext.Consumer>
+        {(value) => {
+          return <div>{value}</div>;
+        }}
+      </AppContext.Consumer>
       <h4>イベント一覧</h4>
       <table className="table table-hover">
         <thead>
